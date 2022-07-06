28943fc4ba149cd94f20cd1be88de13fa8135e2b
Ildaron <ildar.o2010@yandex.ru>
2022-07-06 13:16:48 +0100

    Create 10.Correlationas.py

---
+++ 10.Correlationas.py
@@ -0,0 +1,6 @@
+from scipy.stats import pearsonr    
+print (pearsonr(data_1,data_2))  
+from numpy import cov
+print (cov(data_1, data_2))
+from scipy.stats import spearmanr
+print (spearmanr(data_1, data_2))
